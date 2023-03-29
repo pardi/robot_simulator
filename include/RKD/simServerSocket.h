b15227ec8c268e8873bc6f736ffb4ad430aa8ce8
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-03-30 00:12:40 +0100

    refactor(simServerSocket): refactored the file
    - removed #define and converted them into constexpr

--- include/RKD/simServerSocket.h
+++ include/RKD/simServerSocket.h
@@ -4,14 +4,6 @@
 #ifndef _simServerSocket_header_
 #define _simServerSocket_header_
 
-
-#define PORT 8305
-#define END_MSG "-EndMSG"
-#define ACK_MSG "ACK-EndMSG"
-#define NOACK_MSG "NOACK-EndMSG"
-#define VERBOSE false
-
-
 // Socket Lib
 #include <iostream>
 #include <boost/asio.hpp>
@@ -25,34 +17,78 @@ using ip::tcp;
 
 namespace RKD{
 
+enum class MsgType{
+    ACK = "ACK-EndMSG",
+    NOACK = "NOACK-EndMSG",
+    END = "-EndMSG"};
+
 class simServerSocket{
 public:
 	simServerSocket();
 	~simServerSocket(){};
 
 protected:
-    //Storage
+// Attributes
     boost::asio::io_service service_;
     std::unique_ptr<boost::asio::ip::tcp::acceptor> acceptorPtr_{nullptr}; 
-    bool verbose_{VERBOSE};
+    bool verbose_{false};
     std::list<std::string> msg_in_;
     boost::mutex mux_;
+    constexpr int16_t port_{8305};
 
     //Methods
-    // Wait for a new connection
+    /* /brief Function for waiting for a connection
+     *
+     * This function is used to wait for a connection from a client.
+     *
+     * \return void
+     *
+     */
+
     void waitingForConnection();
 
     // Virtual function for handling the socket message
-    virtual void HandleRequest(boost::asio::ip::tcp::socket&&);
+    /* /brief Virtual function for handling the socket message
+     *
+     * This function is used to handle the message received from the socket.
+     *
+     * \param clientSock The socket used to communicate with the client
+     * \return void
+     *
+     * */
+    virtual void HandleRequest(boost::asio::ip::tcp::socket&& clientSock);
 
-    // Read from the socket a msg
-    std::string readFromSocket(tcp::socket&);
+    /* /brief Read from the socket a msg
+     *
+     * This function is used to read a message from the socket.
+     *
+     * \param socket The socket used to communicate with the client
+     * \return std::string The message read from the socket
+     *
+     * */
+    std::string readFromSocket(tcp::socket& socket);
 
-    // Send to the socket a msg
-    void sendToSocket(tcp::socket&, const std::string&);
+    /* /brief Send to the socket a msg
+     *
+     * This function is used to send a message to the socket.
+     *
+     * \param socket The socket used to communicate with the client
+     * \param msg The message to send to the socket
+     * \return void
+     *
+     * */
+    void sendToSocket(tcp::socket& socket, const std::string& msg
 
-    // Send an Acknowledgement
-    void sendACK(tcp::socket &, const std::string&);
+    /* /brief Send an Acknowledgement
+     *
+     * This function is used to send an Acknowledgement to the socket.
+     *
+     * \param socket The socket used to communicate with the client
+     * \param msg The message to send to the socket
+     * \return void
+     *
+     * */
+    void sendACK(tcp::socket& socket, const std::string& msg);
 
 };
 }
