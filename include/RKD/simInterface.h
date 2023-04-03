618a7557616a0fb963c5cd39f72ae726ce043587
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-04-03 23:50:17 +0100

    refactor(interface): created enums and maps
    created an enums containing all types of actions/function supported. Moreover, we created an `std::unordered_map<>` to map enums to strings

--- include/RKD/simInterface.h
+++ include/RKD/simInterface.h
@@ -18,7 +18,39 @@ using json = nlohmann::json;
 namespace RKD{
 
 class simInterface : public simClientSocket{
+private:
 
+    const std::array<double, 3> defaultColour_{1.0, 1.0, 1.0};
+
+    enum class cmdType{
+        UPDATE_ROBOT,
+        GENERATE_URDF_ROBOT,
+        LOAD_PCD,
+        DRAW_LINE,
+        DRAW_ELLIPSOID,
+        GHOST_ROBOT,
+        ROBOT_TRAJECTORY,
+        DRAW_VECTOR,
+        DRAW_FRAME,
+        DRAW_POINT,
+        SET_BACKGROUND,
+        CLEAR
+    };
+
+    std::unordered_map<cmdType, std::string> cmdMap_{
+            {cmdType::UPDATE_ROBOT, "updateRobot"},
+            {cmdType::GENERATE_URDF_ROBOT, "generateURDFRobot"},
+            {cmdType::LOAD_PCD, "loadPCD"},
+            {cmdType::DRAW_LINE, "drawLine"},
+            {cmdType::DRAW_ELLIPSOID, "drawEllipsoid"},
+            {cmdType::GHOST_ROBOT, "ghostRobot"},
+            {cmdType::ROBOT_TRAJECTORY, "robotTrajectory"},
+            {cmdType::DRAW_VECTOR, "drawVector"},
+            {cmdType::DRAW_FRAME, "drawFrame"},
+            {cmdType::DRAW_POINT, "drawPoint"},
+            {cmdType::SET_BACKGROUND, "setBackground"},
+            {cmdType::CLEAR, "clear"}
+    };
 public:
 	simInterface(){}
 	~simInterface(){}
@@ -30,21 +62,21 @@ public:
 	// Load PCD
 	bool loadPCD(const std::string&, const Eigen::Matrix4f transf = Eigen::Matrix4f::Identity());
 	// Draw Line
-	bool drawLine(const std::vector<Eigen::VectorXd>&, double* colour = new double[3]{1.0, 1.0, 1.0});
+	bool drawLine(const std::vector<Eigen::VectorXd>&, const std::array<double, 3> colour = defaultColour_);
 	// Draw Ellipsoid - parametrisation: ZYX
-	bool drawEllipsoid(const Eigen::Vector3d&, const Eigen::Vector3d&, const Eigen::Vector3d&, const double&, double* colour = new double[3]{1.0, 1.0, 1.0});
+	bool drawEllipsoid(const Eigen::Vector3d&, const Eigen::Vector3d&, const Eigen::Vector3d&, const double, const std::array<double, 3> colour = defaultColour_);
 	// Draw a ghost robot
-	bool ghostRobot(const Eigen::VectorXd&, const double& opacity = 0.8);
+	bool ghostRobot(const Eigen::VectorXd&, const double opacity = 0.8);
 	// Robot Trajectory
-	bool robotTrajectory(const std::vector<Eigen::VectorXd>&, const double& opacity = 0.8);
+	bool robotTrajectory(const std::vector<Eigen::VectorXd>&, const double opacity = 0.8);
 	// Draw a Vector
-	bool drawVector(const Eigen::Vector3d&, const Eigen::Vector3d&, const double& scale = 0.1);
+	bool drawVector(const Eigen::Vector3d&, const Eigen::Vector3d&, const double scale = 0.1);
 	// Draw Frame
 	bool drawFrame(const Eigen::Vector3d&, const Eigen::Vector3d&);
 	// Draw PointFrame
-	bool drawPoint(const Eigen::VectorXd&, const double& size = 4, const double* colour = new double[3]{1.0, 1.0, 1.0});
+	bool drawPoint(const Eigen::VectorXd&, const double size = 4, const std::array<double, 3> colour = defaultColour_);
 	// Colour background
-	bool setBackground(const double&, const double&, const double&);
+	bool setBackground(const double r, const double g, const double b);
 	// Clear scene
 	bool clear();
 
